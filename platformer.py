a0c6b770e935d8ee9ac3e916f870d50258fd290b
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-10-17 13:28:44 -0400

    Updated from Brython Server: 10/17/2016 1:28:43 PM

--- platformer.py
+++ platformer.py
@@ -6,3 +6,18 @@ Assignment:
 Write and submit a program that implements the sandbox platformer game:
 https://github.com/HHS-IntroProgramming/Platformer
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# Background
+black = Color(0x000000, 1)
+white = Color(0xffffff, 1)
+noline = LineStyle(0, black)
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, white)
+bg = Sprite(bg_asset, (0,0))
+
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
